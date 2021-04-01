43aa41e9c8f0fb21ca0be13ec4692503e4231b3d
luozui <329690971@qq.com>
2021-04-01 19:00:06 +0800

    add static

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 func main() {
 	db.Init()
 	r := gin.Default()
-	r.GET("/", server.Index)
+	r.Static("/home", "./static")
+	//r.Static("", "./static")
+	r.GET("/get", server.Index)
 	r.POST("/add", server.Add)
 	r.POST("/opt", server.Opt)
 
 	r.POST("/add_docker", server.AddDocker)
 	r.POST("/opt_docker", server.OptDocker)
 	r.POST("/top_docker", server.TopDocker)
+
 	r.Run(":8080")
 }
